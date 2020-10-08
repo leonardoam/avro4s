090001b93a7f7ae40deef50ccccc5c978ff1b76c
Leonardo Miguel <leonardo.miguel@arquivei.com.br>
2020-10-07 21:47:15 -0300

    Update magnolia to 0.17

--- project/Build.scala
+++ project/Build.scala
@@ -5,29 +5,29 @@ import sbt._
 object Build extends AutoPlugin {
 
   object autoImport {
-    val org = "com.sksamuel.avro4s"
-    val AvroVersion = "1.9.2"
-    val Log4jVersion = "1.2.17"
+    val org              = "com.sksamuel.avro4s"
+    val AvroVersion      = "1.9.2"
+    val Log4jVersion     = "1.2.17"
     val ScalatestVersion = "3.2.2"
-    val Slf4jVersion = "1.7.30"
-    val Json4sVersion = "3.6.9"
-    val CatsVersion = "2.0.0"
+    val Slf4jVersion     = "1.7.30"
+    val Json4sVersion    = "3.6.9"
+    val CatsVersion      = "2.0.0"
     val ShapelessVersion = "2.3.3"
-    val RefinedVersion = "0.9.16"
-    val MagnoliaVersion = "0.16.0"
-    val SbtJmhVersion = "0.3.7"
-    val JmhVersion = "1.23"
+    val RefinedVersion   = "0.9.16"
+    val MagnoliaVersion  = "0.17.0"
+    val SbtJmhVersion    = "0.3.7"
+    val JmhVersion       = "1.23"
   }
 
   import autoImport._
 
   def isGithubActions = sys.env.getOrElse("CI", "false") == "true"
-  def releaseVersion = sys.env.getOrElse("RELEASE_VERSION", "")
-  def isRelease = releaseVersion != ""
+  def releaseVersion  = sys.env.getOrElse("RELEASE_VERSION", "")
+  def isRelease       = releaseVersion != ""
   def githubRunNumber = sys.env.getOrElse("GITHUB_RUN_NUMBER", "local")
-  def ossrhUsername = sys.env.getOrElse("OSSRH_USERNAME", "")
-  def ossrhPassword = sys.env.getOrElse("OSSRH_PASSWORD", "")
-  def publishVersion = if (isRelease) "4.0.0" else "4.1.0." + githubRunNumber + "-SNAPSHOT"
+  def ossrhUsername   = sys.env.getOrElse("OSSRH_USERNAME", "")
+  def ossrhPassword   = sys.env.getOrElse("OSSRH_PASSWORD", "")
+  def publishVersion  = if (isRelease) "4.0.0" else "4.1.0." + githubRunNumber + "-SNAPSHOT"
 
   override def trigger = allRequirements
   override def projectSettings = publishingSettings ++ Seq(
@@ -37,26 +37,27 @@ object Build extends AutoPlugin {
     resolvers += Resolver.mavenLocal,
     parallelExecution in Test := false,
     scalacOptions := Seq(
-      "-unchecked", "-deprecation",
+      "-unchecked",
+      "-deprecation",
       "-encoding",
       "utf8",
-   //   "-Xfatal-warnings",
+      //   "-Xfatal-warnings",
       "-feature",
       "-language:higherKinds",
-   //   "-Xlog-implicits",
+      //   "-Xlog-implicits",
       "-language:existentials",
       "-Ybackend-parallelism",
       "8"
     ),
     javacOptions := Seq("-source", "1.8", "-target", "1.8"),
     libraryDependencies ++= Seq(
-      "org.scala-lang"    % "scala-reflect"     % scalaVersion.value,
-      "org.scala-lang"    % "scala-compiler"    % scalaVersion.value,
-      "org.apache.avro"   % "avro"              % AvroVersion,
-      "org.slf4j"         % "slf4j-api"         % Slf4jVersion          % "test",
-      "log4j"             % "log4j"             % Log4jVersion          % "test",
-      "org.slf4j"         % "log4j-over-slf4j"  % Slf4jVersion          % "test",
-      "org.scalatest"     %% "scalatest"        % ScalatestVersion      % "test"
+      "org.scala-lang"  % "scala-reflect"    % scalaVersion.value,
+      "org.scala-lang"  % "scala-compiler"   % scalaVersion.value,
+      "org.apache.avro" % "avro"             % AvroVersion,
+      "org.slf4j"       % "slf4j-api"        % Slf4jVersion % "test",
+      "log4j"           % "log4j"            % Log4jVersion % "test",
+      "org.slf4j"       % "log4j-over-slf4j" % Slf4jVersion % "test",
+      "org.scalatest"   %% "scalatest"       % ScalatestVersion % "test"
     )
   )
 
